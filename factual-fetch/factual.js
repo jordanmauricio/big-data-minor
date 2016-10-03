fa1d9cc7a1662925cb9deba4f6efaf688382b3c7
Omega5000 <jordanmauricio1@gmail.com>
2016-10-03 10:49:14 +0200

    Exports factual data in JSON format to txt file

--- factual-fetch/factual.js
+++ factual-fetch/factual.js
@@ -1,5 +1,6 @@
 var Factual = require('factual-api');
-var factual = new Factual('KEY', 'SECRET');
+var factual = new Factual('B9DC2ZrFHsqkvlo3C6zrfntMjrcwvH51PVBDFrIf', 'pSAbFJYMcdd9swXOpGPBwhNLGORB3OP1rmKadsna');
+var fs = require('fs');
 var monkey = require('node-monkey').start({host: "127.0.0.1", port:"50500"});
 var express = require('express');
 var app = express();
@@ -18,6 +19,24 @@ app.listen(3434, function(){
 	factual.get('/t/places-us', {q:"starbucks", filters:{"locality":"new york"}}, function (error, res) {
 		console.log('### STARBUCKS ###');
 	  	console.log(res.data);
+	  	//console.log(__dirname);
+
+	  	for (var i = 0; i < res.data.length; i++){
+
+	  		var factualData = JSON.stringify(res.data[i]);
+	  		var jsonFData = factualData + ", ";
+	  		fs.appendFile('factual.txt', jsonFData, (err) => {
+	  			if (err) throw err;
+	  			console.log("It's saved.");
+	  		});
+	  		// var txtFile = __dirname + "data.txt";
+	  		// var file = new File(txtFile);
+	  		// var str = JSON.stringify(res.data[i]);
+
+	  		// var dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(str);
+
+	  		// var link = document.getElementById('link').href = dataUri;
+	  	}
 	});
 
 	//  search restaurants or bars in New York
